b84ce3c76556b93e1847faa7d88b92c264afce5f
Sean Stollhaus <seanstollhaus@Seans-MBP.local>
2022-11-07 21:11:37 -0600

    SQL left

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,17 +1,25 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Employer extends AbstractEntity {
     @NotBlank(message = "Name is required.")
     @Size(min=3,max=75, message="Name must be between 3 and 75 characters.")
     private String location;
+@OneToMany
+@JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
 
-    public Employer (String l) {
-        super();
-        this.location = l;
+    public Employer (String location) {
+
+        this.location = location;
     }
     public Employer() {
 
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,18 +1,29 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class Skill extends AbstractEntity {
 
     @NotBlank(message = "Skill field must be complete.")
     @Size(min=3,max=150, message = "Skill must be between 3 and 150 characters.")
     private String description;
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs = new ArrayList<>();
 
-    public Skill (String d){
-        super();
-        this.description =d;
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+
+    public Skill (String description){
+//        super();
+        this.description =description;
 
     }
     public Skill(){
